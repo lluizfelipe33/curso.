79cd55c3f2522ad31156856a0a7de1736be95a1d
lluizfelipe33 <113647246+lluizfelipe33@users.noreply.github.com>
2022-10-10 20:19:31 -0300

    file

---
+++ grafos.f
@@ -0,0 +1,32 @@
+Suponha que existam seis sistemas computacionais (A, B, C, D, E, e F) interconectados entre si da seguinte forma:
+E
+A
+B
+C
+F D
+➜ Esta informação pode ser representada por este diagrama, chamado de
+grafo.
+➜ Este diagrama identifica unicamente um grafo.
+UFMG/ICEx/DCC MD ·
+Grafos 2
+Motivação
+• Dois objetos especiais:
+– Vértices
+– Arestas
+F
+E
+B
+A C
+D
+Vértice
+Aresta
+UFMG/ICEx/DCC MD ·
+Grafos 3
+Definição
+Um grafo G consiste de dois conjuntos finitos:
+1. Vértices V (G)
+2. Arestas E(G)
+Em geral, um grafo G é representado como:
+G = (V, E)
+UFMG/ICEx/DCC MD ·
+Grafos 
