0a293145f38e34590dd38bed75ff852b6d669a6c
lluizfelipe33 <113647246+lluizfelipe33@users.noreply.github.com>
2022-10-29 09:13:39 -0300

    dados.

---
+++ dados.js
@@ -0,0 +1,11 @@
+Alguns exemplos de dados do nosso cotidiano:
+	Aparelho celular, com eles recebemos dados da internet, ligações e mensagens.
+	Sinais de trânsito, eles nos dados que nos informa se devemos seguir, parar ou ficar em atenção na estrada.
+	Computadores, com eles recebemos dados dos mais variados meios, informações como aulas, filmes, musicas, entre outras..
+
+Alguns exemplos de dados usados na internet:
+	Cadastro em sites de compras.
+	Envio de e-mails.
+	Acesso a plataformas de estudos.
+
+	
